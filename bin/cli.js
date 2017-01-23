fa76c59d8522d5a2d5d0b886fa34450ef5697641
Houfeng <admin@xhou.net>
2017-01-24 01:21:26 +0800

    dev

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const cmdline = require('cmdline');
-const pkg = require('../package.json');
 const fs = require('fs');
 const path = require('path');
-const shify = require('shify');
+const cmdline = require('cmdline');
+const pkg = require('../package.json');
+const core = require('../');
 
 cmdline
   .version(pkg.version)
@@ -13,26 +13,29 @@ cmdline
     console.error(err.message);
     process.exit(1);
   })
+
   /**
    * 创建工程
    */
-  .root.command(['create', 'new'])
-  .option(['-t', '--type'], 'switch')
-  .action(function ($1) {
-    console.log($1);
-    return false;
+  .root.command(['init'])
+  .action(function () {
+    core.init(process.cwd()).catch(err => {
+      console.error(err);
+    });;
   })
+
   /**
    * 其它未明确定义的命令
    * 统一转为 'npm run xxx'
    */
-  // .root.command(/^[a-z]+$/ig)
-  // .action(function (command) {
-  //   var io = shify(function () {
-  //     /* npm run ${command} */
-  //   }, { params: { command: command }, temp: false });
-  //   io.on('exit', process.exit.bind(process));
-  //   io.stdout.pipe(process.stdout);
-  // })
+  .root.command(/^[a-z]+$/ig)
+  .action(function (command) {
+    core.run(command).then(() => {
+      process.exit();
+    }).catch(() => {
+      process.exit();
+    })
+  })
+
   //开始解析
   .ready();
